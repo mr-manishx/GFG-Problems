54e977fb8fa407967c58851de112a18030eddc05
mr-manishx <manishkr10123@gmail.com>
2024-11-03 01:41:48 +0530

    array problems from gfg

---
+++ Arrays_Problems/src/basic_problems/Reverse_Array_in_Groups.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Reverse_Array_in_Groups{
+
+    public static void printArray(ArrayList<Integer> nums){
+        for(int i = 0; i < nums.size(); i++){
+            System.out.print(nums.get(i)+" ");
+        }
+        System.out.println();
+    }
+    public static void reverseArray(ArrayList<Integer> nums, int k){
+
+        int n = nums.size();
+
+        for(int i = 0; i < n; i += k){
+            int end = Math.min(i+k-1, n-1);
+            Collections.reverse(nums.subList(i, end+1));
+        }
+    }
+    public static void main(String[] agrs){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+
+        for(int i = 0; i < n; i++){
+            nums.add(sc.nextInt());
+        }
+        printArray(nums);
+        System.out.println("Output here....");
+        reverseArray(nums, k);
+        printArray(nums);
+    }
+}
